a6d1a0bfbd2c95049d1579f11b3177557c85e43f
Priyank2912 <priyank223017@gmail.com>
2022-01-22 11:44:30 +0530

    More question done for Intuit

---
+++ Intuit/08. Number of Boomerangs.cpp
@@ -0,0 +1,31 @@
+int numberOfBoomerangs(vector<vector<int>> &A)
+{
+
+    int ans = 0;
+
+    for (int i = 0; i < A.size(); ++i)
+    {
+
+        unordered_map<long, int> mp;
+
+        for (int j = 0; j < A.size(); ++j)
+        {
+
+            if (j == i)
+                continue;
+
+            int a = A[i][1] - A[j][1];
+            int b = A[i][0] - A[j][0];
+
+            int c = a * a;
+            c += b * b;
+
+            mp[c]++;
+        }
+
+        for (auto p : mp)
+            ans += p.second * (p.second - 1);
+    }
+
+    return ans;
+}
